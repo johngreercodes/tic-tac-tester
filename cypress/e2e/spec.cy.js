0c23765d9b12ccde75f3a1a127bdf0d6123592a6
John Greer <johnmorrisgreer@gmail.com>
2022-07-29 20:38:47 -0400

    copied code from exercise and initialized cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,5 @@
+describe('first test', () => {
+  it('visits the codepen site', () => {
+    cy.visit('https://codepen.io/CalendlyQA/full/KKPQLmV') // Given the user visits 'https://codepen.io/CalendlyQA/full/KKPQLmV'
+  })
+})
