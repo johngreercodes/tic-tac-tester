77918839193ecc683e26b73d86b53b8baf96d329
John Greer <johnmorrisgreer@gmail.com>
2022-07-30 09:54:12 -0400

    initial test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,11 @@ describe('first test', () => {
   it('visits the codepen site', () => {
     cy.visit('https://john-greer-07-29-22.surge.sh/') // Given the user visits 'https://john-greer-07-29-22.surge.sh/'
   })
+
+  it('loads a 3x3 grid', () => {
+    cy.visit('https://john-greer-07-29-22.surge.sh/')
+    cy.get('#number')
+      .type('3')
+  })
+
 })
